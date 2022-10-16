2aa058d356c342e5f3eaeffecab74ff4d54645a6
MuhammadMuchlisAbimanyu <92872429+MuhammadMuchlisAbimanyu@users.noreply.github.com>
2022-10-16 16:05:20 +0800

    Delete POSTTEST5 directory

--- POSTTEST5/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    $conn = mysqli_connect("localhost", "root", "", "overpriced_coffe");
-
-
-    if (!$conn) {
-        die("Gagal terhubung ke database" . mysqli_connect_error());
-    }
-?>
